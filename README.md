a8cddc6d84f65cea8491a19244e364714fe60e1c
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-19 19:22:37 +0200

    Read me changes and tryout_julius.oy

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sapienza_fds_competition_pokemon_battles_prediction
+
+# Create env
 conda env create -n sapienza_fds_pokemon -f environment.yml
+
+# Activate env
 conda activate sapienza_fds_pokemon
--- notebooks/tryout_julius.py
+++ notebooks/tryout_julius.py
@@ -1,3 +1,37 @@
 from src.utils import config
+import pandas
+import json
 
-print(config.SEED)
+
+train_data = []
+
+# Read the file line by line
+print(f"Loading data from '{config.DATA_TRAIN_PATH}'...")
+try:
+    with open(config.DATA_TRAIN_PATH, 'r') as f:
+        for line in f:
+            # json.loads() parses one line (one JSON object) into a Python dictionary
+            train_data.append(json.loads(line))
+
+    print(f"Successfully loaded {len(train_data)} battles.")
+
+    # Let's inspect the first battle to see its structure
+    print("\n--- Structure of the first train battle: ---")
+    if train_data:
+        first_battle = train_data[0]
+        
+        # To keep the output clean, we can create a copy and truncate the timeline
+        battle_for_display = first_battle.copy()
+        battle_for_display['battle_timeline'] = battle_for_display.get('battle_timeline', [])[:2] # Show first 2 turns
+        
+        # Use json.dumps for pretty-printing the dictionary
+        print(json.dumps(battle_for_display, indent=4))
+        if len(first_battle.get('battle_timeline', [])) > 3:
+            print("    ...")
+            print("    (battle_timeline has been truncated for display)")
+
+
+except FileNotFoundError:
+    print(f"ERROR: Could not find the training file at '{config.DATA_TRAIN_PATH}'.")
+    print("Please make sure you have added the competition data to this notebook.")
+
