18a20ae54dfdd6e7fb9b909e4e945944e0feaa81
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-19 18:52:00 +0200

    changes to config and import structure

---
+++ notebooks/tryout_julius.py
@@ -0,0 +1,4 @@
+import src.utils as utils
+
+print(utils.SEED)
+
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,13 @@
 
+# paths 
+DATA_RAW_DIR = "data/raw/"
+DATA_INTERIM_DIR = "data/interim/"
+DATA_CLEANED_DIR = "data/cleaned/"
 
-DATA_TRAIN_PATH = "data/raw/train.jsonl"
-DATA_TEST_PATH = "data/raw/test.jsonl"
+DATA_TRAIN_PATH = f"{DATA_RAW_DIR}train.jsonl"
+DATA_TEST_PATH = f"{DATA_RAW_DIR}test.jsonl"
+MODEL_DIR = "models/"
+SUBMISSION_DIR = "submissions/"
 
+# seed
 SEED = 1019
