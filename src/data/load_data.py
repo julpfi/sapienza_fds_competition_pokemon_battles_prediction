870d4919aa191a038e44f0868249cfe3fb73f90d
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-20 15:23:23 +0200

    Save local changes before pull

---
+++ src/data/load_data.py
@@ -0,0 +1,17 @@
+from src.utils import config
+import json
+import pandas as pd
+
+
+def load_raw_data() -> list:
+    print(f"Loading raw data from '{config.DATA_TRAIN_PATH}'...")
+    train_data = []
+    try:
+        with open(config.DATA_TRAIN_PATH, 'r') as f:
+            for line in f:
+                train_data.append(json.loads(line))
+    except FileNotFoundError:
+        print(f"ERROR: Could not find the training file at '{config.DATA_TRAIN_PATH}'.")
+    return train_data
+
+def load_data():
