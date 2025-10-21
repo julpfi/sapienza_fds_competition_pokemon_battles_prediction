b541845a9a8c03aac02538a9fefda0c65abbc3b4
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-21 14:03:13 +0200

    added extract methods for different dfs to work with; started with simple structure

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,18 +1,53 @@
-from src.utils import config
 from data.load_data import load_data 
-import json
 import pandas as pd
 
 
+def extract_battles_df(raw_data:list, train: bool=True) -> pd.DataFrame: 
+    battles = []
 
-def clean_data():
-    # Executes all necessary functins from above
+    for x in raw_data:
+        battle = {"battle_id": x["battle_id"]}
+        battle["player_won"]  =  x["player_won"] if train else None  # placeholder for test set
 
-    train_data = load_data()
+        for key, val in x["p2_lead_details"].items():
+            battle[f"p2_lead_{key}"] = val
+
+        battles.append(battle)
+    return  pd.DataFrame(battles)
+
+def extract_turns_df(raw_data: list) -> pd.DataFrame: 
+    turns = []
+    for x in raw_data:
+        for t in x["battle_timeline"]:
+            turn = {"battle_id": x["battle_id"]}
+            for key, val in t.items():
+                turn[f"p2_lead_{key}"] = val
+
+            turns.append(turn)
+    return pd.DataFrame(turns)
+    
+def extract_teams_df(raw_data: list) -> pd.DataFrame: 
+    teams = []
+    for x in raw_data: 
+        team = {"battle_id": x["battle_id"]}
+        for nr, pokemon in enumerate(x["p1_team_details"]): 
+            for key, val in pokemon.items():
+                team[f"p1_{nr}_{key}"] = val
+
+        teams.append(team)
+    return pd.DataFrame(teams)
+
+
+def clean_data(train: bool=True):
+    data = load_data()
+
+    battles = extract_battles_df(data, train)
+    turn = extract_turns_df(data)
+    teams = extract_teams_df(data)
 
     # Somewhere, where we acutally clean data, we need to drop the flawed record row: 4877 
     # Not sure which one is actually the flawed record
-    print(train_data[4877])
+    print(data[4877])
     print("PLACEHOLDER")
 
 
