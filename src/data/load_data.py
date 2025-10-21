73dbcac7d82ec0d8d4966b1931beb421df8a6269
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-21 13:38:44 +0200

    general finish of load data

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,26 +1,23 @@
 from src.utils import config
 import json
-import pandas as pd
 
-
-def load_raw_data(train) -> list:
-    path = config.DATA_TRAIN_PATH if train else config.DATA_TEST_PATH 
-    print(f"Loading raw data from '{path}'...")
-    train_data = []
-    try:
-        with open(path, 'r') as f:
-            for line in f:
-                train_data.append(json.loads(line))
-    except FileNotFoundError:
-        print(f"ERROR: Could not find the training file at '{path}'.")
-    return train_data
-
-def load_data(train: bool=True):
+def load_data(train: bool=True) -> list:
     '''
-    Wrapper: Loads the raw data from the folder /data/raw 
+    Loads the raw data from the folder /data/raw 
     Param:
         train: Boolean - Selectes either the training data for training the model 
             or the test data for predicting the results and submitting   
+    Return: 
+        list of batttles; each battle is a strcuture of dicts and list containing the information 
     '''
-    data_raw = load_raw_data(train)
-    return data_raw
+
+    path = config.DATA_TRAIN_PATH if train else config.DATA_TEST_PATH 
+    print(f"Loading raw data from '{path}'...")
+    data = []
+    try:
+        with open(path, 'r') as f:
+            for line in f:
+                data.append(json.loads(line))
+    except FileNotFoundError:
+        print(f"ERROR: Could not find the training file at '{path}'.")
+    return data
