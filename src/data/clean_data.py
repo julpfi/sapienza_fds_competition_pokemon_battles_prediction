2ccde5e67a4a7b2147d239cd76073c9bcc83fefa
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-10-21 14:58:40 +0200

    clean_data.py added first method decsriptions

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -4,6 +4,15 @@ import pandas as pd
 
 
 def extract_battles_df(raw_data:list, train: bool=True) -> pd.DataFrame: 
+    '''
+    Description: 
+        Extracts each battles player two descriptions and if training set the player_won column (primary key: battle_id)
+        The method collects all the relevant stats for the pokemon on player 2 and flattens the types
+    Param: 
+        list: raw_data that contains the read battles 
+    Return: 
+        Pandas Dataframe with the described records
+    '''
     battles = []
 
     for x in raw_data:
@@ -31,9 +40,20 @@ def extract_turns_df(raw_data: list) -> pd.DataFrame:
                 turn[f"p2_lead_{key}"] = val
 
             turns.append(turn)
+
     return pd.DataFrame(turns)
-    
+
+
 def extract_teams_df(raw_data: list) -> pd.DataFrame: 
+    '''
+    Description: 
+        Extracts the team of player 1 (up to 6 pokemons) where each pokemon is a line (primary keys: battle_id, nr)
+        The method collects all the relevant stats for the pokemons and flattens the types
+    Param: 
+        list: raw_data that contains the read battles 
+    Return: 
+        Pandas Dataframe with the described records
+    '''
     team_pokemons = []
     for x in raw_data:
         battle_id = x["battle_id"]
@@ -56,17 +76,29 @@ def extract_teams_df(raw_data: list) -> pd.DataFrame:
 
 
 
-def clean_data(train: bool=True):
+def clean_data(train: bool=True) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Load, extracts, and cleans the pokemon data
+    Param: 
+        Train: bool: Indicates if it is handling the test or train data and adjusts logic (mainly player_won column)
+    Returns:
+        battles_df: pandas df: One row per battle
+        turns_df: pandas df: One row per turn per battle
+        teams_df: pandas df: One row per pokemon per battle
+    """
+     
     data = load_data()
 
     battles = extract_battles_df(data, train)
-    turn = extract_turns_df(data)
+    turns = extract_turns_df(data)
     teams = extract_teams_df(data)
 
     # Somewhere, where we acutally clean data, we need to drop the flawed record row: 4877 
     # Not sure which one is actually the flawed record
     print(data[4877])
     print("PLACEHOLDER")
+
+    return battles, turns, teams
 
 
  
