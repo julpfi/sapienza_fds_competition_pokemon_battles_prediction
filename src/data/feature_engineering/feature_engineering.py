983e09d0794e4196b9466e81597bbb9130eeb0b6
Ludovico Piras <pirasludovico@gmail.com>
2025-11-02 15:54:22 +0100

    retunr only one df

--- src/data/feature_engineering/feature_engineering.py
+++ src/data/feature_engineering/feature_engineering.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from .features_version_1 import feature_engineering_version_1
+from .features_version_2 import feature_engineering_version_2
 # try if works: from src.data.feature_engineering.features_version_1 import feature_engineering_version_1
 
 def feature_engineering(battles:pd.DataFrame, turns:pd.DataFrame, teams:pd.DataFrame, version: int=1, train:bool=True):
@@ -11,13 +12,10 @@ def feature_engineering(battles:pd.DataFrame, turns:pd.DataFrame, teams:pd.DataF
     match version: 
         case 1: 
             features = feature_engineering_version_1(train, battles, turns, teams)
+        case 2: 
+            features = feature_engineering_version_2(train, battles, turns, teams)
         case _: 
             raise Exception("ERROR: Invalid selection of which set of features to use. \n -> feature_engineering.py")
         
-     # train data will conatin "player_won" column; test does not -> returns accordingly 
-    if train:
-        y = features["player_won"]
-        X = features.drop(columns=["player_won"])
-        return X, y
-    else:
-        return features
+ 
+    return features
