244c4bbbd69f0ea6c0dbcc7798e9ae86dc511969
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-11-03 11:08:21 +0100

    Added xgboost classifier

--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from utils.config import SEED
+from xgboost import XGBClassifier
 
 def get_logistic_regression():
     # creates param dict and sets default values 
@@ -11,6 +12,11 @@ def get_random_forest():
     # creates param dict and sets default values 
     model_params = dict(random_state=SEED, n_estimators=100) 
     return RandomForestClassifier(**model_params)
+
+def get_xgboost():
+    # creates param dict and sets default values 
+    model_params = dict(random_state=SEED)
+    return XGBClassifier(**model_params)
 
 def get_model(model_type: str = "logistic"):
     '''
@@ -24,6 +30,8 @@ def get_model(model_type: str = "logistic"):
         return get_logistic_regression()
     elif model_type == "random_forest":
         return get_random_forest()
+    elif model_type == "xgboost": 
+        return get_xgboost()
     else:
         raise ValueError(f"Unknown model type: {model_type}")
     
