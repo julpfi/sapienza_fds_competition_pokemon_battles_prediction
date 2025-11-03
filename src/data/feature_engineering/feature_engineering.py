e9bb62f86b04eed70cc5391afe6aafc80d499dec
Ludovico Piras <pirasludovico@gmail.com>
2025-11-03 19:08:48 +0100

    added the case 5 in the feature eng file

--- src/data/feature_engineering/feature_engineering.py
+++ src/data/feature_engineering/feature_engineering.py
@@ -3,6 +3,7 @@ from .features_version_1 import feature_engineering_version_1
 from .features_version_2 import feature_engineering_version_2
 from .features_version_3 import feature_engineering_version_3
 from .features_version_4 import feature_engineering_version_4
+from .features_version_5 import feature_engineering_version_5
 
 # try if works: from src.data.feature_engineering.features_version_1 import feature_engineering_version_1
 
@@ -20,7 +21,10 @@ def feature_engineering(battles:pd.DataFrame, turns:pd.DataFrame, teams:pd.DataF
         case 3: 
             features = feature_engineering_version_3(train, battles, turns, teams)   
         case 4: 
-            features = feature_engineering_version_4(train, battles, turns, teams)            
+            features = feature_engineering_version_4(train, battles, turns, teams)  
+        case 5: 
+            features = feature_engineering_version_5(train, battles, turns, teams)             
+                     
         case _: 
             raise Exception("ERROR: Invalid selection of which set of features to use. \n -> feature_engineering.py")
         
