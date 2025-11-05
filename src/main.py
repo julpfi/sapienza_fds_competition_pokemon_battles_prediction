55b1a87e31146ca3a9c1837f556575eae474d536
Julius Pfingsten <julius.pfingsten@gmx.de>
2025-11-05 18:27:35 +0100

    added knn

--- src/main.py
+++ src/main.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
         1 : "logistic",
         2: "random_forest",
         3 : "xgboost",
-        4 : "voting"
+        4: "knn",
+        5 : "voting"
     }
-    model_type = model_map.get(int(input("Select model to use: \n    1 - Logistic regression\n    2 - Random forest\n    3 - XGBoost\n    4 - Voting (1-2)\n>>> ").strip()))
+    model_type = model_map.get(int(input("Select model to use: \n    1 - Logistic regression\n    2 - Random forest\n    3 - XGBoost\n    4 - k Nearest Neighbour\n    5 - Voting (1-4)\n>>> ").strip()))
     with_grid_search = input("With GridSearch (y/n)\n>>> ").strip().lower() == 'y'
     print("\n")
     
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 
 
     # ----------------------------------------------------------------------------------------
-    predict_and_create_csv = input("Predict test data and create csv (y/n)\n>>> ").lower() == 'y'
+    predict_and_create_csv = input("\nPredict test data and create csv (y/n)\n>>> ").strip().lower() == 'y'
 
     if predict_and_create_csv: 
     # 2.1. Load and clean test data
