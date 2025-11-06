2a57ef60e01bf46705b471cff6c3fca8a3a19434
Ludovico Piras <pirasludovico@gmail.com>
2025-11-06 11:45:53 +0100

    added features version 6, edited feature engineering file

--- src/data/feature_engineering/feature_engineering.py
+++ src/data/feature_engineering/feature_engineering.py
@@ -4,6 +4,7 @@ from .features_version_2 import feature_engineering_version_2
 from .features_version_3 import feature_engineering_version_3
 from .features_version_4 import feature_engineering_version_4
 from .features_version_5 import feature_engineering_version_5
+from .features_version_6 import feature_engineering_version_6
 
 # try if works: from src.data.feature_engineering.features_version_1 import feature_engineering_version_1
 
@@ -23,7 +24,9 @@ def feature_engineering(battles:pd.DataFrame, turns:pd.DataFrame, teams:pd.DataF
         case 4: 
             features = feature_engineering_version_4(train, battles, turns, teams)  
         case 5: 
-            features = feature_engineering_version_5(train, battles, turns, teams)             
+            features = feature_engineering_version_5(train, battles, turns, teams)       
+        case 6: 
+            features = feature_engineering_version_6(train, battles, turns, teams)                  
                      
         case _: 
             raise Exception("ERROR: Invalid selection of which set of features to use. \n -> feature_engineering.py")
